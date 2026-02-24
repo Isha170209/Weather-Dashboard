ee166d545b521618cb90bd0bd8ed0e68650436de
Isha170209 <ishku1022@gmail.com>
2026-02-24 16:21:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -193,7 +193,7 @@ if st.session_state.submitted and st.session_state.lat and st.session_state.lon:
             if all_data.empty:
                 st.warning("No historical data to plot.")
             else:
-                fig, ax = plt.subplots(figsize=(10, 4))
+                fig, ax = plt.subplots(figsize=(8, 2))
                 ax.plot(all_data["date"], all_data[parameter], marker='o')
                 ax.set_xlabel("Date")
                 ax.set_ylabel(parameter.capitalize())
